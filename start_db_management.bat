14da63203fcf458756b994f399452fe501ba75e0
Neko Sunami <windbreak339@gmail.com>
2025-10-29 06:59:01 +0000

    update start_db_management.bat.
    Signed-off-by: Neko Sunami <windbreak339@gmail.com>

--- start_db_management.bat
+++ start_db_management.bat
@@ -1,25 +1,13 @@
 @echo off
-set PORT=8082    REM 修改端口为8082
-set URL=http://localhost:%PORT%/db_management
-
-start "" "%URL%"
-
-setlocal
-
-REM 检查是否已经安装依赖
-python -c "import flask, fastapi, uvicorn, openai, mysql.connector, PIL, colorama, requests" 2>nul
-if errorlevel 1 (
-    echo 缺少依赖，正在安装...
-    pip install -r requirements.txt
-)
+set PORT=8082
+set URL=http://localhost:%PORT%/db_console
 
 REM 启动Web服务器
-start "WebServer" /D "%CD%" python web_server.py
+cd /d "%~dp0"
+start "WebServer" python -c "import sys; sys.path.insert(0, '.'); from src.web_server import run_web_server; run_web_server()"
 
 REM 等待服务器启动
-ping 127.0.0.1 -n 5 > nul
+timeout /t 5 /nobreak >nul
 
 REM 打开数据库管理页面
 start "" %URL%
-
-endlocal
